f2eae69685d26ec69f4dfb8c8075436465a0977d
WikWitkowski <wiktor.witkowski@safekiddo.com>
2025-04-23 07:45:40 +0200

    Update Rules.swift

--- Sources/SMSFilterSDK/Rules.swift
+++ Sources/SMSFilterSDK/Rules.swift
@@ -17,4 +17,25 @@ public struct Rules: Decodable {
         self.promoKeywords = promoKeywords
         self.transactionKeywords = transactionKeywords
     }
+
+    /// Wbudowany zestaw przykładowych reguł
+    public static var example: Rules {
+        return Rules(
+            blockedSenders: [
+                "+48111222333", "SpamFirm", "800123456", "+447700900123", "LOTTOBOT"
+            ],
+            spamKeywords: [
+                "wygrałeś", "darmowy", "pilne", "konkurs", "bitcoin",
+                "zyskaj", "okazja", "kliknij", "prezent", "voucher"
+            ],
+            promoKeywords: [
+                "promocja", "zniżka", "sale", "black friday",
+                "kod rabatowy", "wyprzedaż", "kupon", "mega promocja"
+            ],
+            transactionKeywords: [
+                "kod weryfikacyjny", "potwierdzenie", "transakcja",
+                "saldo", "rachunek", "faktura", "płatność"
+            ]
+        )
+    }
 }
